bc036e9730bbaa7f3032ff6a9159af617f8b68cb
FV <fevega@gmail.com>
2019-03-05 09:11:54 -0500

    Add questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/basics'
+
+  get 'static_pages/ruby'
+
+  get 'static_pages/git'
+
   root 'application#hello'
 end
