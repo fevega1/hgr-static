b1113d2efae1172d8fa06c72a89bfb6fef576890
FV <fevega@gmail.com>
2019-03-05 09:39:13 -0500

    Add current time display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Hgr Static App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
+  def current_time()
+    current_time = Time.now.strftime("%m/%d/%Y %H:%M:%S");
+  end
+
 end
